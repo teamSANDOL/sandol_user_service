aa0e4eb661f89608f72c60503d9eb2aabf36a76a
Seokyoung-Hong <tjrdud0330@naver.com>
2025-03-27 19:46:57 +0900

    fix: 사용자 직무 계정 필드 목록에서 쉼표 누락 수정

--- users/serializers.py
+++ users/serializers.py
@@ -5,7 +5,7 @@ from .models import User
 class UserSerializer(serializers.ModelSerializer):
     class Meta:
         model = User
-        fields = ['id', 'email', 'name', 'nickname', 'global_admin', 'is_service_account' 'created_at']
+        fields = ['id', 'email', 'name', 'nickname', 'global_admin', 'is_service_account', 'created_at']
         read_only_fields = ['id', 'created_at', 'global_admin', 'is_service_account']
         extra_kwargs = {
             'nickname': {'required': False},
