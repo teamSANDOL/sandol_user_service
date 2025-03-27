69d8ea83f8ca7c05d6a50d0ef44712dceaea126a
Seokyoung-Hong <tjrdud0330@naver.com>
2025-03-27 19:53:36 +0900

    feat: 사용자 관리에서 'global_admin' 필드를 추가하여 목록 표시 및 검색 필드 수정

--- users/admin.py
+++ users/admin.py
@@ -3,6 +3,6 @@ from .models import User
 
 @admin.register(User)
 class UserAdmin(admin.ModelAdmin):
-    list_display = ('id', 'email', 'name', 'nickname', 'created_at', 'service_account')
-    search_fields = ('email', 'name', 'nickname', 'service_account')
+    list_display = ('id', 'email', 'name', 'nickname', 'created_at', 'global_admin', 'service_account')
+    search_fields = ('email', 'name', 'nickname', 'global_admin', 'service_account')
     ordering = ('email',)
